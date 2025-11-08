d9a56b8e2840b8f9a82661f8d44251100f6263c2
abdurahman <abdurrahmanzerihun@gmail.com>
2025-11-08 23:25:05 +0300

    Final commit: Proper indentation

--- script.js
+++ script.js
@@ -1,109 +1,112 @@
-const bookContainer=document.querySelector('.book-container');
-const dialog=document.querySelector('.dialog');
-const addBook=document.querySelector('#add-book');
-const closeButton=document.querySelector('#close-button');
-const bookForm=document.querySelector('#bookForm')
+const bookContainer = document.querySelector('.book-container');
+const dialog = document.querySelector('.dialog');
+const addBook = document.querySelector('#add-book');
+const closeButton = document.querySelector('#close-button');
+const bookForm = document.querySelector('#bookForm');
 
+const myLibrary = [];
 
-const myLibrary=[];
-
-function Book(title,author,year,pages,isRead){
-this.title=title,
-this.author=author,
-this.year=year,
-this.pages=pages,
-this.isRead=isRead,
-this.id=crypto.randomUUID()
+function Book(title, author, year, pages, isRead) {
+    this.title = title,
+    this.author = author,
+    this.year = year,
+    this.pages = pages,
+    this.isRead = isRead,
+    this.id = crypto.randomUUID();
 }
 
-Book.prototype.readStatus=function(bookIsRead){
-bookIsRead.addEventListener('click',()=>{
-       
-        if(this.isRead){
-              
-                this.isRead=!this.isRead;
-                bookIsRead.textContent='Not read';
-                bookIsRead.style.backgroundColor="#6c757d";
+Book.prototype.readStatus = function (bookIsRead) {
+    bookIsRead.addEventListener('click', () => {
+        if (this.isRead) {
+            this.isRead = !this.isRead;
+            bookIsRead.textContent = 'Not read';
+            bookIsRead.style.backgroundColor = "#6c757d";
+        } else {
+            this.isRead = !this.isRead;
+            bookIsRead.textContent = 'Read';
+            bookIsRead.style.backgroundColor = "purple";
         }
-        else{
-                this.isRead=!this.isRead;
-                bookIsRead.textContent='Read';
-                bookIsRead.style.backgroundColor="purple";
-        }
-});
+    });
+};
 
-}
 // function for creating and adding book objects in to the array
-function addBookToLibrary(title,author,year,pages,isRead){
-      let newBook=new Book(title,author,year,pages,isRead) ;
-      myLibrary.push(newBook);
+function addBookToLibrary(title, author, year, pages, isRead) {
+    let newBook = new Book(title, author, year, pages, isRead);
+    myLibrary.push(newBook);
 }
 
-//event for dialog box
-addBook.addEventListener('click',()=> dialog.showModal());
-closeButton.addEventListener('click',()=>dialog.close());
+// event for dialog box
+addBook.addEventListener('click', () => dialog.showModal());
+closeButton.addEventListener('click', () => dialog.close());
 
-//event for form
-bookForm.addEventListener('submit',function(event){
-     event.preventDefault();
-const titleInpt=document.querySelector('#title').value;
-const authorInpt=document.querySelector('#author').value;  
-const yearInpt=document.querySelector('#year').value; 
-const pagesInpt=document.querySelector('#pages').value; 
-const isReadInpt=document.querySelector('#isRead').checked; 
+// event for form
+bookForm.addEventListener('submit', function (event) {
+    event.preventDefault();
 
-addBookToLibrary(titleInpt,authorInpt,yearInpt,pagesInpt,isReadInpt);
-showBook();
-dialog.close();
+    const titleInpt = document.querySelector('#title').value;
+    const authorInpt = document.querySelector('#author').value;
+    const yearInpt = document.querySelector('#year').value;
+    const pagesInpt = document.querySelector('#pages').value;
+    const isReadInpt = document.querySelector('#isRead').checked;
 
+    addBookToLibrary(titleInpt, authorInpt, yearInpt, pagesInpt, isReadInpt);
+    showBook();
+    dialog.close();
 });
+
 // remove books from the array
-function removeBook(id){
-                const index=myLibrary.findIndex(book=>book.id===id);
-                if(index!==-1){
-                        myLibrary.splice(index,1);//remove from [index] only one element(1) 
-                }
-                console.log(myLibrary);
-                showBook();
-        }
-//show books on the page
-function showBook(){
-        bookContainer.innerHTML=" ";//To clear the previous books and add only the last one
-        myLibrary.forEach(book=>{
-        const bookCard=document.createElement('div');
+function removeBook(id) {
+    const index = myLibrary.findIndex(book => book.id === id);
+    if (index !== -1) {
+        myLibrary.splice(index, 1); // remove from [index] only one element(1)
+    }
+    console.log(myLibrary);
+    showBook();
+}
+
+// show books on the page
+function showBook() {
+    bookContainer.innerHTML = " "; // To clear the previous books and add only the last one
+    myLibrary.forEach(book => {
+        const bookCard = document.createElement('div');
         bookCard.classList.add('book-card');
-        const bookTitle=document.createElement('h3');
-        bookTitle.textContent=book.title;
-        const bookAuthor=document.createElement('p');
-        bookAuthor.textContent= `By:${book.author}`;
-        const bookYear=document.createElement('p');
-        bookYear.textContent=`Year : ${book.year}`;
-        const bookPages=document.createElement('p');
-        bookPages.textContent=`Pages:${book.pages}`;
-        const bookIsRead=document.createElement('button');
+
+        const bookTitle = document.createElement('h3');
+        bookTitle.textContent = book.title;
+
+        const bookAuthor = document.createElement('p');
+        bookAuthor.textContent = `By: ${book.author}`;
+
+        const bookYear = document.createElement('p');
+        bookYear.textContent = `Year: ${book.year}`;
+
+        const bookPages = document.createElement('p');
+        bookPages.textContent = `Pages: ${book.pages}`;
+
+        const bookIsRead = document.createElement('button');
         bookIsRead.classList.add('book-read-status');
-        bookIsRead.textContent=book.isRead?"Read":"Not read";
+        bookIsRead.textContent = book.isRead ? "Read" : "Not read";
 
-        bookCard.dataset.id=book.id;          //assign the the book id as data attribute to each book object card
-        const removeBtn=document.createElement('button');
+        bookCard.dataset.id = book.id; // assign the book id as data attribute to each book object card
+
+        const removeBtn = document.createElement('button');
         removeBtn.classList.add('remove-button');
-        removeBtn.textContent="Remove";
-        if(book.isRead){
-                bookIsRead.style.backgroundColor="purple";
+        removeBtn.textContent = "Remove";
+
+        if (book.isRead) {
+            bookIsRead.style.backgroundColor = "purple";
+        } else {
+            bookIsRead.style.backgroundColor = "#6c757d";
         }
-        else{
-                bookIsRead.style.backgroundColor="#6c757d";
-        }
 
-        removeBtn.addEventListener('click',()=>{
-                const bookId=bookCard.dataset.id;
-                bookCard.remove();//remove the card from the view
-                removeBook(bookId);//remove the object from the array
+        removeBtn.addEventListener('click', () => {
+            const bookId = bookCard.dataset.id;
+            bookCard.remove(); // remove the card from the view
+            removeBook(bookId); // remove the object from the array
+        });
 
-        })
+        book.readStatus(bookIsRead); // Call for the toggle
 
-     book.readStatus(bookIsRead);//Call for the toggle
-     
         bookCard.appendChild(bookTitle);
         bookCard.appendChild(bookAuthor);
         bookCard.appendChild(bookYear);
@@ -111,22 +114,12 @@ function showBook(){
         bookCard.appendChild(bookIsRead);
         bookCard.appendChild(removeBtn);
         bookContainer.appendChild(bookCard);
-       
-})
-
+    });
 }
 
-//Default values
-addBookToLibrary('To kill a Mockingbird','Harper Lee',1960,281,'Read');
-addBookToLibrary('The Great Gatsby','F. Scott Fitzgerald',1925,180,'Read'); 
+// Default values
+addBookToLibrary('To kill a Mockingbird', 'Harper Lee', 1960, 281, 'Read');
+addBookToLibrary('The Great Gatsby', 'F. Scott Fitzgerald', 1925, 180, 'Read');
 
-//Call to show all books in the array
+// Call to show all books in the array
 showBook();
-
-
- 
-
-
-
-
-
